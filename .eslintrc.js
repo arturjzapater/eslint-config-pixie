d08eaf1b1a039137f38cf517ad67a634c64fed66
Artur J Zapater <savagepixie@protonmail.com>
2020-07-11 17:53:38 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        'array-bracket-spacing': [ 'error', 'always', {
+            objectsInArrays: false,
+        }],
+        'comma-dangle': [ 'warn', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+        }],
+        'eol-last': [ 'error', 'always' ],
+        'no-prototype-builtins': 0,
+        'no-unused-vars': [ 'error', {
+            args: 'after-used',
+            'argsIgnorePattern': '^_',
+        }],
+        'object-curly-spacing': [ 'error', 'always' ],
+        'semi': [ 'error', 'never' ],
+    },
+}
